6f8b008a6e9f018c5c6118a62e429780e9ed34fc
nyius <josephdesigned@gmail.com>
2022-04-08 00:35:48 -0400

    First commit

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function UserItem({ user: { login, avatar_url } }) {
+	return (
+		<Link to={`/user/${login}`}>
+			<div className="card shadow-md compact side bg-base-100 hover:bg-gray-900">
+				<div className="flex-row items-center space-x-4 card-body">
+					<div>
+						<div className="avatar">
+							<div className="rounded-full shadow w-14 h-14">
+								<img src={avatar_url} alt="user Image" />
+							</div>
+						</div>
+					</div>
+					<div>
+						<h2 className="card-title">{login}</h2>
+						<p className="text-base-content text-opacity-40">Visit Profile</p>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
+}
+
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
